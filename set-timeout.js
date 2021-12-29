07b710a7e20a997d175cfc944e91a46203587673
zhafiara <japicobaaja@gmail.com>
2021-12-29 23:53:20 +0700

    backend-task10

---
+++ set-timeout.js
@@ -0,0 +1,13 @@
+console.log("Fia Membuka Bowser Mocrosoft Edge.");
+
+/**
+ * setTimeout salah satu operasi Asynchronous.
+ * fungsi setTimeout tidak mencegah operasi selanjutnya.
+ * callback otomatis dijalankan setelah 5 detik.
+ */
+setTimeout(() => {
+  console.log("Downloading 1 Hour ...");
+  console.log("Download complete");
+}, 5000);
+
+console.log("Fia membuka Channel Youtube Web Programming UNPAS.");
---
+++ synchronous.js
@@ -0,0 +1,9 @@
+console.log("Fia Membuka Bowser Mocrosoft Edge.");
+
+console.log(`
+Fia download video programming selama 1 jam.
+  Downloading 1 Hour ...
+  Download Complete.
+`);
+
+console.log("Fia membuka Channel Youtube Web Programming UNPAS.");
